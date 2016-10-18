d59bb42a9e31718047d97e229f22ec4c208ce040
Staffan Olsson <staffan@repos.se>
2016-10-18 08:20:08 +0200

    Fixes run as default user after migration to Debian. Custom ZOO_USER is unfixed.

--- 3.4.9/docker-entrypoint.sh
+++ 3.4.9/docker-entrypoint.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [ "$1" = 'zkServer.sh' -a "$(id -u)" = '0' ]; then
+if [ "$1" = 'zkServer.sh' -a "$(id)" = '0' ]; then
+    echo "Current user is: $(id -n) ($(id))"
     chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR"
     exec su-exec "$ZOO_USER" "$0" "$@"
 fi
