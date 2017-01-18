44a28611707f7468245f44a405b996ca6c71c629
Enrique Garcia <engapa@gmail.com>
2017-01-18 18:30:05 +0100

    New configuration parameters from environment variables: ZOO_MAX_CLIENT_CNXNS ZOO_AUTOPURGE_SANPRETAINCOUNT ZOO_AUTOPURGE_PURGEINTERVAL
    This closes #9

--- 3.4.9/docker-entrypoint.sh
+++ 3.4.9/docker-entrypoint.sh
@@ -20,6 +20,10 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
     echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
     echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
 
+    echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
+    echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SANPRETAINCOUNT" >> "$CONFIG"
+    echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL" >> "$CONFIG"
+
     for server in $ZOO_SERVERS; do
         echo "$server" >> "$CONFIG"
     done
