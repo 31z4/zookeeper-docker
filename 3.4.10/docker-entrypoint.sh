0b40b58f76f0f240c815bca020be07d00b559946
Matthew Hertz <squimmy.squimmy@hotmail.com>
2017-11-07 17:56:39 +0000

    Fix if in 3.4.10 - use && rather than -a

--- 3.4.10/docker-entrypoint.sh
+++ 3.4.10/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [[ "$1" = 'zkServer.sh' -a "$(id -u)" = '0' ]]; then
+if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR"
     exec su-exec "$ZOO_USER" "$0" "$@"
 fi
