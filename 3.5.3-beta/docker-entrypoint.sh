a1f98a12777218dcc9652debd8155d2b136b5db7
mehertz <squimmy.squimmy@hotmail.com>
2017-11-08 15:06:00 +0000

    Use [[ instead of [ in docker-entrypoint.sh (#26)
    * Resolve #25

--- 3.5.3-beta/docker-entrypoint.sh
+++ 3.5.3-beta/docker-entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [ "$1" = 'zkServer.sh' -a "$(id -u)" = '0' ]; then
+if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR"
     exec su-exec "$ZOO_USER" "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
-if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
+if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     CONFIG="$ZOO_CONF_DIR/zoo.cfg"
 
     echo "dataDir=$ZOO_DATA_DIR" >> "$CONFIG"
@@ -22,7 +22,7 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
     echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
     echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED" >> "$CONFIG"
 
-    if [ -z $ZOO_SERVERS ]; then
+    if [[ -z $ZOO_SERVERS ]]; then
       ZOO_SERVERS="server.1=localhost:2888:3888;$ZOO_PORT"
     fi
 
@@ -32,7 +32,7 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
 fi
 
 # Write myid only if it doesn't exist
-if [ ! -f "$ZOO_DATA_DIR/myid" ]; then
+if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
     echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
 fi
 
