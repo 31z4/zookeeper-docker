01d134a4dcb1676d7efe9d0c64c5e0cfdba6fb27
Elisey Zanko <elisey.zanko@gmail.com>
2019-05-25 10:33:23 +0500

    Resolve #63: Update to 3.5.5
    * Migrate to openjdk:8-jre-slim

--- 3.5.5/docker-entrypoint.sh
+++ 3.5.5/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-    exec su-exec "$ZOO_USER" "$0" "$@"
+    exec gosu "$ZOO_USER" "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
