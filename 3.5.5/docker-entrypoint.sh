72d02ca4d42bdd6d6c1450d2d8166de7b2899e71
Elisey Zanko <elisey.zanko@gmail.com>
2019-06-08 09:16:20 +0500

    Explicitly set UID/GID when creating a user
    * Get rid of ZOO_USER env variable

--- 3.5.5/docker-entrypoint.sh
+++ 3.5.5/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-    exec gosu "$ZOO_USER" "$0" "$@"
+    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    exec gosu zookeeper "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
