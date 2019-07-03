0c7407b14ec1ee1e29d6f177ffd8e5681fa41131
S.Cavallo <smcavallo@hotmail.com>
2019-07-03 11:23:31 -0400

    add support for advanced configuration via ENV

--- 3.5.5/docker-entrypoint.sh
+++ 3.5.5/docker-entrypoint.sh
@@ -32,6 +32,39 @@ if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     for server in $ZOO_SERVERS; do
         echo "$server" >> "$CONFIG"
     done
+
+    # https://zookeeper.apache.org/doc/current/zookeeperAdmin.html#sc_advancedConfiguration
+    if [[ -z $ZOO_GLOBAL_OUTSTANDING_LIMIT ]]; then
+      echo "globalOutstandingLimit=$ZOO_GLOBAL_OUTSTANDING_LIMIT" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_PRE_ALLOC_SIZE ]]; then
+      echo "preAllocSize=$ZOO_PRE_ALLOC_SIZE" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_CLIENT_PORT_ADDRESS ]]; then
+      echo "clientPortAddress=$ZOO_CLIENT_PORT_ADDRESS" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_MIN_SESSION_TIMEOUT ]]; then
+      echo "minSessionTimeout=$ZOO_MIN_SESSION_TIMEOUT" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_MAX_SESSION_TIMEOUT ]]; then
+      echo "maxSessionTimeout=$ZOO_MAX_SESSION_TIMEOUT" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_FSYNC_WARNING_THRESHOLD_MS ]]; then
+      echo "fsync.warningthresholdms=$ZOO_FSYNC_WARNING_THRESHOLD_MS" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_SYNC_ENABLED ]]; then
+      echo "syncEnabled=$ZOO_SYNC_ENABLED" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_EXTENDED_TYPES_ENABLED ]]; then
+      echo "zookeeper.extendedTypesEnabled=$ZOO_EXTENDED_TYPES_ENABLED" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_EMULATE_353_TTL_NODES ]]; then
+      echo "zookeeper.emulate353TTLNodes=$ZOO_EMULATE_353_TTL_NODES" >> "$CONFIG"
+    fi
+    if [[ -z $ZOO_SERVER_CNXN_FACTORY ]]; then
+      echo "serverCnxnFactory=$ZOO_SERVER_CNXN_FACTORY" >> "$CONFIG"
+    fi
+
 fi
 
 # Write myid only if it doesn't exist
