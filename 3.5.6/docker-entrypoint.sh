cd35f02ac2046789613711cce51aa13035b03dc6
Bablzz <stuffsweep@gmail.com>
2020-01-09 09:07:27 +0300

    Update docker-entrypoint.sh 3.5.6
    4 spaces indentation

--- 3.5.6/docker-entrypoint.sh
+++ 3.5.6/docker-entrypoint.sh
@@ -12,18 +12,18 @@ fi
 if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     CONFIG="$ZOO_CONF_DIR/zoo.cfg"
     {
-      echo "dataDir=$ZOO_DATA_DIR" 
-      echo "dataLogDir=$ZOO_DATA_LOG_DIR"
+        echo "dataDir=$ZOO_DATA_DIR" 
+        echo "dataLogDir=$ZOO_DATA_LOG_DIR"
 
-      echo "tickTime=$ZOO_TICK_TIME"
-      echo "initLimit=$ZOO_INIT_LIMIT"
-      echo "syncLimit=$ZOO_SYNC_LIMIT"
+        echo "tickTime=$ZOO_TICK_TIME"
+        echo "initLimit=$ZOO_INIT_LIMIT"
+        echo "syncLimit=$ZOO_SYNC_LIMIT"
 
-      echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
-      echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
-      echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
-      echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
-      echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
+        echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
+        echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
+        echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
+        echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
+        echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
     } >> "$CONFIG"
     if [[ -z $ZOO_SERVERS ]]; then
       ZOO_SERVERS="server.1=localhost:2888:3888;2181"
