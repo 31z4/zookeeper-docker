d646d7c658c57593efa35785e7aae29cd7d5b0d4
Jon Stelly <967068+jonstelly@users.noreply.github.com>
2020-02-06 16:03:43 -0600

    Support for multi-arch
    - openjdk:8-slim only supports amd64
    - adoptopenjdk:latest supports amd64, arm64, arm/v7, ppc64le, s390x
    
    - TODO: This includes a bump from jre 8 to jre 11.  What testing is required?
    - TODO: gosu doesn't work for buildx / qemu-user arm emulation so I switched to su command.  Are there issues with this?  I don't think gosu is needed for a simple switch user command
    
    Signed-off-by: Jon Stelly <967068+jonstelly@users.noreply.github.com>

--- 3.5.6/docker-entrypoint.sh
+++ 3.5.6/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-    exec gosu zookeeper "$0" "$@"
+    exec su zookeeper "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
