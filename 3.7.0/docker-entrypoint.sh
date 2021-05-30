1b908cf415307f4c2f35a6add068402999df8546
Elisey Zanko <elisey.zanko@gmail.com>
2021-05-30 10:17:24 +0500

    Fix #106: Do not chown $ZOO_CONF_DIR when container starts (#107)

--- 3.7.0/docker-entrypoint.sh
+++ 3.7.0/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR"
     exec gosu zookeeper "$0" "$@"
 fi
 
